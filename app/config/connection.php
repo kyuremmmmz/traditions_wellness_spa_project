37178f480caec353b19c22a23d3400e4eca925f6
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-01-22 12:02:14 +0800

    added connection

--- app/config/connection.php
+++ app/config/connection.php
@@ -1,6 +1,18 @@
 <?php
-
+header('Content-Type: application-json');
 $db = 'traditionswellnessspa';
 $password = 'root';
 $username = 'admin';
-$host = '';
+$host = 'localhost:3307';
+
+$pdo = new PDO("mysql:host=$host;dbname=$db", $username, $password);
+if (!$pdo) {
+    header('Content-Type: application-json');
+    echo json_encode([
+        'errorMessage' => 'not connected'
+    ]);
+}else{
+    echo json_encode([
+        'message' => 'connected'
+    ]);
+}
