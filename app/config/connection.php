875b79126191a2f90dae31b12bba8c246803e1d6
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-01-22 12:11:07 +0800

    main

--- app/config/connection.php
+++ app/config/connection.php
@@ -1,18 +1,26 @@
 <?php
+session_start();
 header('Content-Type: application-json');
-$db = 'traditionswellnessspa';
-$password = 'root';
-$username = 'admin';
-$host = 'localhost:3307';
-
-$pdo = new PDO("mysql:host=$host;dbname=$db", $username, $password);
-if (!$pdo) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     header('Content-Type: application-json');
-    echo json_encode([
-        'errorMessage' => 'not connected'
-    ]);
+    $db = 'traditionswellnessspa';
+    $password = 'root';
+    $username = 'admin';
+    $host = 'localhost:3307';
+
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $username, $password);
+    if (!$pdo) {
+        header('Content-Type: application-json');
+        echo json_encode([
+            'errorMessage' => 'not connected'
+        ]);
+    } else {
+        echo json_encode([
+            'message' => 'connected'
+        ]);
+    }
 }else{
     echo json_encode([
-        'message' => 'connected'
+        'message' => 'This method should be a POST method'
     ]);
 }
