1659d079aa8ff99a6a0c0f991cda72642b6d254a
Pen Diaz <stephendiaz.syv@gmail.com>
2025-02-04 14:57:41 +0800

    fixed the header -rheiann

--- app/views/php/pages/forgotpassword/page.php
+++ app/views/php/pages/forgotpassword/page.php
@@ -2,8 +2,10 @@
 namespace Project\App\Views\Php\Pages\ForgotPassword;
 
 use Project\App\Views\Php\Components\Buttons\ReturnButton;
+use Project\App\Views\Php\Components\Containers\Header;
 use Project\App\Views\Php\Components\Inputs\GlobalInputField;
 use Project\App\Views\Php\Components\Inputs\InputField;
+use Project\App\Views\Php\Components\Texts\HeaderTwo;
 
 
 class Page
@@ -14,21 +16,42 @@ class Page
         $emailError = $_SESSION['forgot_password_errors']['email'] ?? '';
         unset($_SESSION['forgot_password_errors']);
 
+
+
+
+
+
+        // start of page
+                    Header::render('Small');
+        echo        '<main class="OneColumnContainer mt-[80px] sm:mt-[100px] bg-background dark:bg-darkBackground">';
+                        HeaderTwo::render('Forgot Password');
+
+        echo        '</main>';
+
+
         echo <<<HTML
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <div class="flex items-center justify-center min-h-screen">
             <div class="flex flex-col items-center w-full max-w-md px-4">
                 <!-- Return Button -->
                 <div class="self-start mb-8">
 HTML;
         
-        $returnButton = new ReturnButton(
-            "Return to Login", 
-            "button",  
-            16, 
-            true,
-            "window.location.href = '/login'"
-        );
-        echo $returnButton->render();
+
+ 
 
         echo <<<HTML
                 </div>
@@ -48,8 +71,6 @@ HTML;
                 <form method="POST" action="/forgot" class="w-full max-w-xs space-y-6">
 HTML;
         
-        $emailField =  GlobalInputField::render('email', 'Please enter your email', 'email', 'email');
-        echo '<div class="w-full">' . $emailField . '</div>';
 
         echo <<<HTML
                     <button type="submit" class="w-full py-3 text-white transition-colors bg-blue-600 rounded-md hover:bg-blue-700">
