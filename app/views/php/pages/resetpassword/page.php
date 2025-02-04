7ee62ca5cedb671862f9c3d2a0ff536b081c4fb9
iann16 <rhpa.ico.up@phinmaed.com>
2025-02-04 18:10:08 +0800

    reset password rheiann

--- app/views/php/pages/resetpassword/page.php
+++ app/views/php/pages/resetpassword/page.php
@@ -14,7 +14,22 @@ class Page{
 
 
         $passwordError = "";
-        $eightCharacters = "onBackgroundTwo";
+
+        $eightCharactersCircle = "onBackgroundTwo";
+        $eightCharactersText = "onBackgroundTwo";
+
+        $specialCharacterCircle = "onBackgroundTwo";
+        $specialCharacterText = "onBackgroundTwo";
+
+        $uppercaseLetterCircle = "onBackgroundTwo";
+        $uppercaseLetterText = "onBackgroundTwo";
+
+        $lowercaseLetterCircle = "onBackgroundTwo";
+        $lowercaseLetterText = "onBackgroundTwo";
+
+        $numberCircle = "onBackgroundTwo";
+        $numberText = "onBackgroundTwo";
+
         $firstName = 'user';
 
         // start of page
@@ -25,26 +40,26 @@ class Page{
                             HeaderTwo::render('Hello, ' . $firstName .'', 'onBackground', 'darkOnBackground', '', '[316px]', '[40px]', '',  '[8px]');
                             BodyTwo::render("Please enter your new password below.", 'onBackgroundTwo','darkOnBackgroundTwo','','[316px]','','','[12px]');
                             GlobalInputField::render("password", "Password", "password", "new_password_field", $passwordError);
-        echo                '<div>';
+        echo                '<div class="w-[284px] px-[14px]">';
                                 CaptionOne::render('Your password must contain at least:','onBackgroundTwo', 'darkonBackgroundTwo',  '','[284px]', '[12px]');
         echo                    '<div class="flex items-center gap-[7px]">';
-                                    IconChoice::render('miniCircle','[8px]','[8px]',$eightCharacters,);
+                                    IconChoice::render('miniCircle','[8px]','[8px]',$eightCharactersCircle,);
                                     CaptionOne::render('Eight characters','onBackgroundTwo', 'darkonBackgroundTwo',  '','[284px]', '[2px]');
         echo                    '</div>';
         echo                    '<div class="flex items-center gap-[7px]">';
-                                    IconChoice::render('miniCircle','[8px]','[8px]',$eightCharacters,);
+                                    IconChoice::render('miniCircle','[8px]','[8px]',$specialCharacterCircle,);
                                     CaptionOne::render('One special character (e.g., @, #, $, %).','onBackgroundTwo', 'darkonBackgroundTwo',  '','[284px]', '[2px]');
         echo                    '</div>';
         echo                    '<div class="flex items-center gap-[7px]">';
-                                    IconChoice::render('miniCircle','[8px]','[8px]',$eightCharacters,);
+                                    IconChoice::render('miniCircle','[8px]','[8px]',$uppercaseLetterCircle,);
                                     CaptionOne::render('One upperrcase letter  (A-Z).','onBackgroundTwo', 'darkonBackgroundTwo',  '','[284px]', '[2px]');
         echo                    '</div>';
         echo                    '<div class="flex items-center gap-[7px]">';
-                                    IconChoice::render('miniCircle','[8px]','[8px]',$eightCharacters,);
+                                    IconChoice::render('miniCircle','[8px]','[8px]',$lowercaseLetterCircle,);
                                     CaptionOne::render('One lowercase letter (a-z).','onBackgroundTwo', 'darkonBackgroundTwo',  '','[284px]', '[2px]');
         echo                    '</div>';
         echo                    '<div class="flex items-center gap-[7px]">';
-                                    IconChoice::render('miniCircle','[8px]','[8px]',$eightCharacters,);
+                                    IconChoice::render('miniCircle','[8px]','[8px]',$numberCircle,);
                                     CaptionOne::render('One number (0-9).','onBackgroundTwo', 'darkonBackgroundTwo',  '','[284px]', '[2px]');
         echo                    '</div>';
         echo                '</div>';
