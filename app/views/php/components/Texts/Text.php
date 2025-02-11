5f79e4310e90c56a7e1c019e37e2b4640bd69058
pennie <stephendiaz.syv@gmail.com>
2025-02-11 15:09:54 +0800

    text

--- app/views/php/components/Texts/Text.php
+++ app/views/php/components/Texts/Text.php
@@ -4,7 +4,7 @@ namespace Project\App\Views\Php\Components\Texts;
 
 class Text
 {
-    public static function render(string $id, string $name, string $class, $content): void
+    public static function render(?string $id, ?string $name, string $class, $content): void
     {
         echo <<<HTML
         <p id="{$id}" name="{$name}" class="{$class}">
