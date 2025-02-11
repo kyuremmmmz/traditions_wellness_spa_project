c3cecc3ecb8a93d4aaa20b4c5e8b4f97ed238d7c
Sabado, Crystal Jade C. <crce.sabado.up@phinmaed.com>
2025-02-11 15:29:08 +0800

    forgotpassword

--- app/views/php/components/Buttons/ReturnButton.php
+++ app/views/php/components/Buttons/ReturnButton.php
@@ -6,7 +6,7 @@ use Project\App\Views\Php\Components\Icons\IconChoice;
 
 class ReturnButton
 {
-    public static function render(string $width = "", string $goToPage): void
+    public static function render(string $width = "", string $goToPage = ""): void
     {
         echo        '<button class="w-{$width} h-[14px]" onclick="goToPage()">';
                         IconChoice::render("leftArrow", "[14px]", "[14px]", '', 'onBackground', 'darkOnBackground', 'background', 'darkBackground', '', '');
