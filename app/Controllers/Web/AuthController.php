f792e03068a066b205e990a05e49b75e0e9269d6
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-02-12 11:03:27 +0800

    ✅: Remember me logic already done

--- app/Controllers/Web/AuthController.php
+++ app/Controllers/Web/AuthController.php
@@ -149,6 +149,9 @@ class AuthController
                     'email' => $response['email'],
                     'photos' => $response['photos'],
                 ];
+                if (!empty($_POST['remember_me'])) {
+                    setcookie('remember_cookie', base64_encode(json_encode($_SESSION['user']['role'])), time() + 3600);
+                }
                 setcookie('user', base64_encode(json_encode($_SESSION['user'])), time() + 3600, '/');
                 if (is_null($response['email_verified_at'])) {
                     $this->controller->update(
--- app/views/php/components/Inputs/RememberMe.php
+++ app/views/php/components/Inputs/RememberMe.php
@@ -10,7 +10,7 @@ class RememberMe {
         <div class="flex gap-[10px]"> 
                 <div class="flex justify-center items-center">
                 <label class="w-[24px] h-[24px]">
-                    <input class="peer cursor-pointer hidden after:opacity-100" type="checkbox"/>
+                    <input class="peer cursor-pointer hidden after:opacity-100" type="checkbox" name="$name"/>
                     <span class="inline-block w-[24px] h-[24px] border-[2px] border-borderTwo dark:border-darkBorderTwo relative rounded-[6px] cursor-pointer after:content-[''] after:absolute after:top-2/4 after:left-2/4 after:-translate-x-1/2 after:-translate-y-1/2 after:w-[12px] after:h-[12px] after:bg-primary dark:after:bg-darkPrimary after:rounded-[2px] after:opacity-0 peer-checked:after:opacity-100"></span>
                 </label>
             </div>
