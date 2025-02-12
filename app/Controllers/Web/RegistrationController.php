60bcc57c3c10fc6e103e60a84c89e93743c9a01a
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-02-12 17:18:56 +0800

    added

--- app/Controllers/Web/RegistrationController.php
+++ app/Controllers/Web/RegistrationController.php
@@ -26,8 +26,8 @@ class RegistrationController
     private function generateTemporaryUserNameAndPassword($firstName, $lastName)
     {
         $baseUserName  = strtolower(Str($firstName . '.' . $lastName));
-        $randomNum = rand(1000, 9999);
-        $temporaryPassword = 'Temp' . $randomNum;
+        $randomNum = rand(100000, 999999);
+        $temporaryPassword = $randomNum;
         return [
             'username' => $baseUserName . $randomNum,
             'password' => $temporaryPassword,
