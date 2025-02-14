af815bd4906ac7ce1024092352ce8f25d7466f6d
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2025-02-14 20:47:07 +0800

    fixed the dom

---
+++ app/Controllers/Web/ContinueRegistrationController.php
@@ -0,0 +1,39 @@
+<?php
+
+class ContinueRegistrationController
+{
+    public function index()
+    {
+        // Code for listing resources
+        echo "This is the index method of ContinueRegistrationController.";
+    }
+
+    public function create()
+    {
+        // Code for showing a create form
+        echo "This is the create method of ContinueRegistrationController.";
+    }
+
+    public function store()
+    {
+        // Code for storing new resources
+        echo "This is the store method of ContinueRegistrationController.";
+    }
+
+    public function continueRegistrationFunction()
+    {
+        
+    }
+
+    public function update($id)
+    {
+        // Code for updating resources
+        echo "This is the update method of ContinueRegistrationController for ID: $id.";
+    }
+
+    public function delete($id)
+    {
+        // Code for deleting resources
+        echo "This is the delete method of ContinueRegistrationController for ID: $id.";
+    }
+}
---
+++ app/models/ResetPasswordModel.php
@@ -0,0 +1,43 @@
+<?php
+
+class ResetPasswordModel
+{
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function getAll()
+    {
+        $stmt = $this->pdo->query("SELECT * FROM your_table_name");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function find($id)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM your_table_name WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function create($data)
+    {
+        $stmt = $this->pdo->prepare("INSERT INTO your_table_name (column1, column2) VALUES (:value1, :value2)");
+        return $stmt->execute($data);
+    }
+
+    public function update($id, $data)
+    {
+        $stmt = $this->pdo->prepare("UPDATE your_table_name SET column1 = :value1, column2 = :value2 WHERE id = :id");
+        $data['id'] = $id;
+        return $stmt->execute($data);
+    }
+
+    public function delete($id)
+    {
+        $stmt = $this->pdo->prepare("DELETE FROM your_table_name WHERE id = :id");
+        return $stmt->execute(['id' => $id]);
+    }
+}
