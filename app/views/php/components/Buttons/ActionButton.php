07df0b72ceb509f4330fd0358749564745468925
pennie <stephendiaz.syv@gmail.com>
2025-03-21 15:01:06 +0800

    Responsive appointments

--- app/views/php/components/Buttons/ActionButton.php
+++ app/views/php/components/Buttons/ActionButton.php
@@ -13,7 +13,7 @@ class ActionButton
         IconChoice::render($iconChoice, '16px', '16px', '', 'onPrimary', 'darkOnPrimary');
         $iconHtml = ob_get_clean();
 
-        echo '<button class="text-onPrimary dark:text-darkOnPrimary transition-all ease-in-out BodyTwo bg-primary dark:bg-darkPrimary hover:bg-primaryHover dark:hover:bg-darkPrimaryHover disabled:bg-secondaryVariant rounded-[6px] w-[219px] h-[40px]" id="' . $id . '">
+        echo '<button class="text-onPrimary dark:text-darkOnPrimary transition-all ease-in-out BodyTwo bg-primary dark:bg-darkPrimary hover:bg-primaryHover dark:hover:bg-darkPrimaryHover disabled:bg-secondaryVariant rounded-[6px] w-[219px] min-w-[219px] h-[40px]" id="' . $id . '">
             <div class="flex gap-[8px] w-[full] items-center justify-center">
                 ' . $iconHtml . '
                 ' . $content . '
