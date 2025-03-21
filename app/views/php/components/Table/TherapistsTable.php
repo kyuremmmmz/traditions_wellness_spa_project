5ee08bab689ceda8ea457a52fcf7b05ca3941b18
pennie <stephendiaz.syv@gmail.com>
2025-03-21 16:49:48 +0800

    ewan

--- app/views/php/components/Table/TherapistsTable.php
+++ app/views/php/components/Table/TherapistsTable.php
@@ -6,21 +6,17 @@ class TherapistsTable
 {
     public static function render(string $id, ?string $className = null): void
     {
-        $tableClasses = "w-full table-fixed" . ($className ?? '');
-        echo "<div class='w-full overflow-x-auto bg-background dark:bg-darkBackground min-w-[300px] h-[400px] overflow-y-auto'>";
+        $tableClasses = "w-full table-fixed overflow-x-auto" . ($className ?? '');
+        echo "<div class='w-full overflow-x-auto bg-background dark:bg-darkBackground min-w-[1362px] h-[500px] overflow-y-auto overflow-x-auto'>";
         echo "<table class='$tableClasses'>";
-        echo "<thead class='text-onBackgroundTwo dark:text-darkOnBackgroundTwo uppercase bg-[#FFEA06] bg-opacity-5 tracking-wider px-[48px] mx-[48px]'>
+        echo "<thead class='text-onBackgroundTwo dark:text-darkOnBackgroundTwo uppercase bg-[#FFEA06] bg-opacity-5 tracking-wider px-[48px] mx-[48px] overflow-x-auto'>
                 <tr>
                     <th scope='col' class='pl-[48px] pr-[8px] py-4 CaptionTwo w-[72px] text-center'>No.</th>
                     <th scope='col' class='py-4 px-[8px] CaptionTwo w-[160px] text-center'>Therapist name</th>
                     <th scope='col' class='py-4 px-[8px] CaptionTwo w-[160px] text-center'>Status</th>
                     <th scope='col' class='py-4 px-[8px] CaptionTwo w-[80px] text-center'>Gender</th>
                     <th scope='col' class='py-4 px-[8px] CaptionTwo w-[160px]'>email</th>
-                </tr>
-
-                
-
-                
+                </tr>                
             </thead>";
         echo "<tbody id='$id' class='divide-y divide-gray-100'>";
         echo "</tbody></table></div>";
